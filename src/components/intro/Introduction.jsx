60694b3c532d0d3a648ccfb8158180fe250b88ad
Iamvictorsam <iamvictorsam@gmail.com>
2022-09-11 20:31:13 +0100

    did some changes

--- src/components/intro/Introduction.jsx
+++ src/components/intro/Introduction.jsx
@@ -4,20 +4,20 @@ import "./intro-style.css";
 const Introduction = () => {
   return (
     <section
-    className="intro-section"
+      className="intro-section"
       data-scroll-section
       data-scroll
       data-scroll-speed="6"
     >
+      <div className="intro-image">
+        <img src={image} height={700} alt="profile" />
+      </div>
       <div className="intro-title">
         <h1 data-scroll data-scroll-speed="9">
           Isaac Junior
           <br />A Frontend Engineer and Technical writer with a deep focus on
           creating pitch-perfect designs
         </h1>
-      </div>
-      <div className="intro-image">
-        <img src={image} height={700} alt="profile" />
       </div>
     </section>
   );
